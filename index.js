aaef9f9ee39d019e2fc8b1ed3b3f0db2a1e152fa
Kyle Clapper <kpuc1997@gmail.com>
2019-04-19 15:04:55 -0400

    Changed to port 80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 80;
 const path = require('path');
 const router = express.Router();
 const fs = require('fs');
